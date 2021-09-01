ffa4cf16d5591a9af159242c6237e93a19ec6aaa
Chuck LU <chucklu2015@outlook.com>
2021-09-01 18:48:24 +0800

    add a unit test project based on .net 5

---
+++ CoreUnitTest/CoreUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CoreUnitTest
+{
+    [TestFixture]
+    public class CoreUnitTest
+    {
+        [Test]
+        public void Test20210901_001()
+        {
+            Console.WriteLine("hello world!");
+        }
+    }
+}
