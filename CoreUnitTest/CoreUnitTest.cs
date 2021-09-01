f635fce3ab739439e80cdf610870ff44298ad881
Chuck LU <chucklu2015@outlook.com>
2021-09-01 18:50:07 +0800

    add json serialize case

--- CoreUnitTest/CoreUnitTest.cs
+++ CoreUnitTest/CoreUnitTest.cs
@@ -11,5 +11,17 @@ namespace CoreUnitTest
         {
             Console.WriteLine("hello world!");
         }
+
+        [Test]
+        public void Test20210901_002()
+        {
+            //https://stackoverflow.com/a/58504215/13338936
+            var text = "<script>alert('o hai');</script>";
+
+            var json = System.Text.Json.JsonSerializer.Serialize(new { Property = text });
+
+            Console.WriteLine(json);
+            //{"Property":"\u003Cscript\u003Ealert(\u0027o hai\u0027);\u003C/script\u003E"}
+        }
     }
 }
