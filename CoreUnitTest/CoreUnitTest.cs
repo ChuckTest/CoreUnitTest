392e3722e5700ea76e6898b850110f09fe66d25c
Chuck LU <chucklu2015@outlook.com>
2021-09-01 19:03:40 +0800

    add test case

--- CoreUnitTest/CoreUnitTest.cs
+++ CoreUnitTest/CoreUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using NUnit.Framework;
 
@@ -31,6 +32,20 @@ namespace CoreUnitTest
         public void Test20210901_003()
         {
             var text = "{\"FullName\":\"\\u003Cscript\\u003Ealert(\\u0027o hai\\u0027);\\u003C/script\\u003E\"}";
+
+            var employee = JsonSerializer.Deserialize<Employee>(text);
+
+            Console.WriteLine(employee?.FullName);
+            //<script>alert('o hai');</script>
+        }
+
+        [Test]
+        public void Test20210901_004()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+
+                var text = "{\"FullName\":\"\\u003Cscript\\u003Ealert(\\u0027o hai\\u0027);\\u003C/script\\u003E\"}";
 
             var employee = JsonSerializer.Deserialize<Employee>(text);
 
